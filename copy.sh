54415088847962a926371eae3407fb5c4fed3b81
cuu <gnubsd@me.com>
2023-08-08 15:06:22 +1200

    copy.sh add .xinitrc

--- copy.sh
+++ copy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cp /usr/local/bin/skel/.xinitrc /home/cpi/ -rf
+
 cp /usr/local/bin/skel/.config /home/cpi/ -rf
 
 cp -rf /usr/local/bin/skel/.dosbox /home/cpi
@@ -7,6 +9,7 @@ cp -rf /usr/local/bin/skel/.dosbox /home/cpi
 cp -rf /usr/local/bin/skel/.opentyrian  /home/cpi
 
 cp -rf /usr/local/bin/skel/.local /home/cpi
+
 
 sudo chown -R cpi:cpi /home/cpi
 
