61baf9787d798fe215b5cfce3af7bcb9368baa7c
cuu <gnubsd@me.com>
2023-08-08 15:04:48 +1200

    copy.sh

---
+++ copy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp /usr/local/bin/skel/.config /home/cpi/ -rf
+
+cp -rf /usr/local/bin/skel/.dosbox /home/cpi
+
+cp -rf /usr/local/bin/skel/.opentyrian  /home/cpi
+
+cp -rf /usr/local/bin/skel/.local /home/cpi
+
+sudo chown -R cpi:cpi /home/cpi
+
